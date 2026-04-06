2706831723188e27de4e53b74a47cfdea87720f8
Jorge-Biarnes <jorge.h.biarnes@gmail.com>
2026-04-06 18:22:41 -0300

    primer commit - script Python y dataset ventas

---
+++ extraccion query para python.sql
@@ -0,0 +1,31 @@
+SELECT
+	DD.[FullDateAlternateKey] AS DATE,
+	FIS.[SalesOrderNumber] AS ORDER_NUMBER,
+    FIS.[SalesAmount] AS SALES_AMOUNT,
+	DC.[FirstName] AS NAME,
+	DC.[LastName] AS LAST_NAME,
+	DP.[EnglishProductName] AS PRODUCT_NAME,
+	DPC.[EnglishProductCategoryName] AS PRODUCT_CATEGORY_NAME,
+	DG.[City],
+	DG.[EnglishCountryRegionName] AS COUNTRY
+
+FROM [AdventureWorksDW2022].[dbo].[FactInternetSales] FIS
+INNER JOIN [AdventureWorksDW2022].[dbo].[DimDate] DD
+ON  DD.[DateKey] = FIS.[OrderDateKey]
+
+INNER JOIN [AdventureWorksDW2022].[dbo].[DimCustomer] DC
+ON DC.[CustomerKey] = FIS.[CustomerKey]
+
+INNER JOIN [AdventureWorksDW2022].[dbo].[DimProduct] DP
+ON DP.[ProductKey] = FIS.[ProductKey]
+
+INNER JOIN [AdventureWorksDW2022].[dbo].[DimProductSubcategory] DPS
+ON DPS.[ProductSubcategoryKey] = DP.[ProductSubcategoryKey]
+
+INNER JOIN [AdventureWorksDW2022].[dbo].[DimProductCategory] DPC
+ON DPC.[ProductCategoryKey] = DPS.[ProductCategoryKey]
+
+INNER JOIN [AdventureWorksDW2022].[dbo].[DimGeography] DG
+ON DG.[GeographyKey] = DC.[GeographyKey]
+
+ORDER BY DD.[FullDateAlternateKey] DESC
